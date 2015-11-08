61bad99a2597413d3b780d6935e8c335acea082b
Taylor <taylor@Taylors-Mac.local>
2015-11-08 13:12:31 -0500

    My commit message

---
+++ PizzaAppleWatch WatchKit Extension/pizzza.swift
@@ -0,0 +1,16 @@
+//
+//  pizzza.swift
+//  PizzaAppleWatch
+//
+//  Created by Taylor on 11/8/15.
+//  Copyright © 2015 Taylor. All rights reserved.
+//
+
+import WatchKit
+
+class pizzza: NSObject {
+    var tamSeleccion = "Chica"
+    var tipoDeMasa = "Delgada"
+    var tipoDeQueso = "Cheddar"
+    var ingredientes = ["": false]
+}
